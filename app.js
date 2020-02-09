72165bcfc8331f98347fe61449893fdc99e05130
JJY-CodeMonkey <dmsqlctnekf@gmail.com>
2020-02-09 12:34:42 +0900

    static, body-parser, post, email

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.listen(3000, function() {
     console.log("start! express server on port 3000");
 });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/public/index.html");
@@ -17,5 +21,32 @@ app.get('/main', function(req, res) {
 
 app.post('/email_post', function(req, res) {
     //get : req.param('email')
-   res.send("post response");
+    // console.log(req.body.email);
+    // res.send("<h1>welcome " + req.body.email +  "</h1>" );
+    res.render('email.ejs', {'email': req.body.email});
 });
+
+app.post("/ajax_send_email", function(req, res){
+    console.log(req.body.email);
+
+    let responseData = {
+        'result': '',
+        'detail': {
+            'data': {}
+        },
+        
+    }
+
+    if(req.body.email !== "") {
+        console.log("break if");
+
+        responseData['result'] = 'success';
+        responseData['detail']['data']['email'] = req.body.email;
+    } else {
+        console.log("break else");
+        responseData['result'] = 'fail';
+        responseData['detail']['data'] = null;
+    }
+
+    res.json(responseData);
+})

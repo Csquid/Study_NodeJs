c7d3f2ac8a214ec8499dfd89c76b7f63ba22a807
JJY-CodeMonkey <rmaqlctnekf@naver.com>
2020-02-12 23:22:51 +0900

    mysql backend END

--- router/member.js
+++ router/member.js
@@ -8,15 +8,35 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'lake0019',
+    user: '',
     password: '1234',
     database: 'ns_db'
 }) ;
 
 connection.connect();
 
-router.get('/', function(req, res) {
+router.get('/join', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/join.html'));
 });
 
+router.post('/join', function(req, res) {
+    const body = req.body;
+
+    const reqDataObject = {
+        id: body.id,
+        pw: body['pw-1'],
+        name: body.name,
+        email: body.email,
+        address: body.address,
+        gender: body.gender
+    }
+
+    let query = connection.query('INSERT INTO MEMBER SET ?', reqDataObject, function(error, results, fields) {
+        if(error) throw error;
+
+        console.log("ok insert db ", results);
+    });
+    
+    // console.log(reqDataObject);
+});
 module.exports = router;
